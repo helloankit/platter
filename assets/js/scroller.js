c4514ef0972c8d64165caf6d1d9c1fdd6c425834
helloankit <c.ankit1997@gmail.com>
2022-12-22 01:32:20 +0530

    feat: dark mode + snap scroll

---
+++ assets/js/scroller.js
@@ -0,0 +1,12 @@
+// docSlider.init();
+
+// // change the speed
+// docSlider.init({
+//     speed : 500
+// });
+
+// // change the speed and easing
+// docSlider.init({
+//     speed : 500,
+//     easing : 'ease-in-out'
+// });
